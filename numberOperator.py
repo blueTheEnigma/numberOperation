9d09d27d438f424a00fdbbf08b9c0ce1226a558f
Morgan Ufuoma <136262923+blueTheEnigma@users.noreply.github.com>
2023-06-26 14:50:31 +0100

    Add files via upload

---
+++ numberOperator.py
@@ -0,0 +1,70 @@
+def zero(*func): 
+    #your code here
+    if len(func) == 0:
+        return 0
+    else:
+        (exp,) = func
+        return int(eval(str(0)+exp))
+def one(*func): #your code here
+     if len(func) == 0:
+        return 1
+     else:
+        (exp,) = func
+        return int(eval(str(1)+exp))
+def two(*func): #your code here
+     if len(func) == 0:
+        return 2
+     else:
+        (exp,) = func
+        return int(eval(str(2)+exp))
+def three(*func): #your code here
+     if len(func) == 0:
+        return 3
+     else:
+        (exp,) = func
+        return int(eval(str(3)+exp))
+def four(*func): #your code here
+     if len(func) == 0:
+        return 4
+     else:
+        (exp,) = func
+        return int(eval(str(4)+exp))
+def five(*func): #your code here
+     if len(func) == 0:
+        return 5
+     else:
+        (exp,) = func
+        return int(eval(str(5)+exp))
+def six(*func): #your code here
+     if len(func) == 0:
+        return 6
+     else:
+        (exp,) = func
+        return int(eval(str(6)+exp))
+def seven(*func): #your code here
+     if len(func) == 0:
+        return 7
+     else:
+        (exp,) = func
+        return int(eval(str(7)+exp))
+def eight(*func): #your code here
+     if len(func) == 0:
+        return 8
+     else:
+        (exp,) = func
+        return int(eval(str(8)+exp))
+def nine(*func): #your code here
+     if len(func) == 0:
+        return 9
+     else:
+        (exp,) = func
+        return int(eval(str(9)+exp))
+
+def plus(args): #your code here
+    return '+'+str(args)
+def minus(agrs): #your code here
+    return '-'+str(agrs)
+def times(agrs): #your code here
+    return '*'+str(agrs)
+def divided_by(agrs): #your code here
+    return '*'+str(agrs)
